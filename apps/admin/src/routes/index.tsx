2b6af3e585807d2eb75cdd3136f1b8aee486b97d
Melvyn Claes <claes.melvyn@gmail.com>
2026-01-09 13:00:52 +0100

    feat: add @melv1c/ui-kit in both frontend and admin

--- apps/admin/src/routes/index.tsx
+++ apps/admin/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { useAPIHealth } from '@/hooks/use-api-health';
-import { Button } from '@repo/ui';
+import { Button } from '@melv1c/ui-kit';
+import { Logo } from '@repo/ui';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
@@ -12,15 +13,11 @@ function Index() {
   return (
     <div className="p-2">
       <h3>Admin Dashboard</h3>
-      <Button
-        onClick={() => alert('Welcome to the Admin Dashboard!')}
-        className="mt-2 bg-blue-500 text-white p-2 rounded cursor-pointer"
-      >
-        Click me
-      </Button>
+      <Logo />
       {isPending && <p className="mt-2">Loading API health...</p>}
       {isError && <p className="mt-2 text-red-500">Failed to fetch API health</p>}
       {data && <pre className="mt-2">{JSON.stringify(data, null, 2)}</pre>}
+      <Button onClick={() => alert('Button from UI Kit clicked!')}>Click Me</Button>
     </div>
   );
 }
--- apps/frontend/src/routes/index.tsx
+++ apps/frontend/src/routes/index.tsx
@@ -1,6 +1,7 @@
-import { Button } from '@repo/ui';
-import { createFileRoute } from '@tanstack/react-router';
 import { useAPIHealth } from '@/hooks/use-api-health';
+import { Button } from '@melv1c/ui-kit';
+import { Logo } from '@repo/ui';
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
   component: Index,
@@ -12,12 +13,7 @@ function Index() {
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
-      <Button
-        onClick={() => alert('Hello from the Button component!')}
-        className="mt-2 bg-blue-500 text-white p-2 rounded cursor-pointer"
-      >
-        Click me
-      </Button>
+      <Logo />
 
       <div className="mt-4">
         <h4>API Health Check:</h4>
@@ -25,6 +21,8 @@ function Index() {
         {isError && <p className="text-red-500">Error fetching API health.</p>}
         {data && <p className="text-green-500">API is healthy!</p>}
       </div>
+
+      <Button onClick={() => alert('Button from UI Kit clicked!')}>Click Me</Button>
     </div>
   );
 }
