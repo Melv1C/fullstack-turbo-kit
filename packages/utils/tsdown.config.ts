e3eb6933f9b1de2542bfde2d417bb5e9f208ca29
Melvyn Claes <claes.melvyn@gmail.com>
2026-02-26 22:24:19 +0100

    chore: remove unused tsdown configuration file

--- packages/utils/tsdown.config.ts
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from 'tsdown';
-
-export default defineConfig({
-  clean: false, // We need to keep the dist folder for the backend to work, so we won't clean it before building
-});
